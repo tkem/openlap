f06a1223cff5802795de337e72e36bfdb34a3f4d
Thomas Kemmer <tkemmer@computer.org>
2016-08-12 20:43:00 +0200

    Add cordova-plugin-app-version required by ionic-native.

--- app/pages/settings/about/about.ts
+++ app/pages/settings/about/about.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { AppVersion } from 'ionic-native';
 
+import { Logger } from '../../../providers';
+
 @Component({
   templateUrl: 'build/pages/settings/about/about.html',
 })
@@ -9,8 +11,11 @@ export class AboutPage {
 
   version: Promise<string>;
 
+  constructor(private logger: Logger) {}
+
   ngOnInit() {
     this.version = AppVersion.getVersionNumber().catch(error => {
+      this.logger.error('Error retrieving application version', error);
       return 'develop';
     });
   }
