549ad7e83a8b450a04ce97d7f13adcb734a2a74f
Thomas Kemmer <tkemmer@computer.org>
2016-09-09 15:58:06 +0200

    Race settings UI improvements.

--- app/pages/race-settings/race-settings.ts
+++ app/pages/race-settings/race-settings.ts
@@ -1,49 +1,41 @@
 import { Component } from '@angular/core';
 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { NavParams, ViewController } from 'ionic-angular';
 
-function anyRequired(...names: string[]) {
-  return (group: FormGroup): {[key: string]: any} => {
-    for (let name of names) {
-      if (!Validators.required(group.controls[name])) {
-        return null;
-      }
-    }
-    return {'required': true};
-  }
+function formatTime(milliseconds: number) {
+  const h = Math.floor(milliseconds / 3600000);
+  const m = Math.floor(milliseconds / 60000 % 60);
+  const s = Math.floor(milliseconds / 1000 % 60);
+  return [h, m, s].map(v => ('0' + v).substr(-2)).join(':');
 }
 
-function formatTime(milliseconds) {
-  let s = Math.floor(milliseconds / 1000);
-  let result = '';
-  while (s) {
-    const f = s % 60;
-    result = f + result;
-    s = Math.floor(s / 60);
-    if (s) {
-      if (f < 10) {
-        result = '0' + result;
-      }
-      result = ':' + result;
-    }
-  }
-  return result;
-}
-
-function parseTime(s) {
+function parseTime(s: string) {
   return s.split(':').reduce((sum, cur) => sum * 60 + parseInt(cur), 0) * 1000;
 }
 
-const timeValidator = Validators.pattern('((([0-9]+\:)?[0-5])?[0-9]\:)?[0-5][0-9]');
+function timeRequired(control: AbstractControl): {[key: string]: any} {
+  if (!control.value || !parseTime(control.value)) {
+    return {'required': true};
+  } else {
+    return null;
+  }
+}
+
+function lapsOrTimeRequired(group: FormGroup): {[key: string]: any} {
+  if (parseInt(group.controls['laps'].value)) {
+    return null;
+  }
+  if (!timeRequired(group.controls['time'])) {
+    return null;
+  }
+  return {'required': true};
+}
 
 function createQualifyingForm(fb: FormBuilder, params: NavParams) {
   return fb.group({
-    time: [
-      formatTime(params.get('time') || 180000), 
-      [timeValidator, Validators.required]
-    ],
+    time: [formatTime(params.get('time') || 180000), timeRequired],
     auto: [params.get('auto') || false],
     pace: [params.get('pace') || false]
   });
@@ -51,13 +43,13 @@ function createQualifyingForm(fb: FormBuilder, params: NavParams) {
 
 function createRaceForm(fb: FormBuilder, params: NavParams) {
   return fb.group({
-    laps: [params.get('laps') || 10],
-    time: [formatTime(params.get('time')), timeValidator],
+    laps: [params.get('laps') || 10, Validators.pattern('\\d*')],
+    time: [formatTime(params.get('time'))],
     auto: [params.get('auto') || false],
     pace: [params.get('pace') || false],
     slotmode: [params.get('slotmode') || false]
   }, {
-    validator: anyRequired('laps', 'time')
+    validator: lapsOrTimeRequired
   });
 }
 
