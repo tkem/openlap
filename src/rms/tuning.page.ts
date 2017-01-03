adadfd505decf97b0d45ffd99652cfa7f152b768
Thomas Kemmer <tkemmer@computer.org>
2017-01-03 12:38:45 +0100

    Fix #49: Improve tuning page interface.

--- src/rms/tuning.page.ts
+++ src/rms/tuning.page.ts
@@ -4,15 +4,14 @@ import { Observable, Subject } from 'rxjs';
 
 import { ControlUnit } from '../carrera';
 import { CONTROL_UNIT_PROVIDER, Driver, Settings } from '../core';
-import { Logger } from '../logging';
 
 // TODO: store with CU or settings?
 // TODO: initial values, mark as touched, etc.
 const MODELS = [0, 1, 2, 3, 4, 5].map(id => ({
   id: id,
-  speed: 8,
-  brake: 8,
-  fuel: 8
+  speed: null,
+  brake: null,
+  fuel: null
 }));
 
 @Component({
@@ -25,11 +24,7 @@ export class TuningPage {
 
   models = MODELS;
 
-  all = {
-    speed: 8,
-    brake: 8,
-    fuel: 8
-  };
+  link = false;
 
   type = 'speed';
 
@@ -37,13 +32,13 @@ export class TuningPage {
 
   private subject = new Subject<{type: string, id: number}>();
 
-  constructor(private cu: ControlUnit, private ref: ChangeDetectorRef, private logger: Logger, settings: Settings) {
+  constructor(private cu: ControlUnit, private ref: ChangeDetectorRef, settings: Settings) {
     this.drivers = settings.getDrivers();
 
     // TODO: is this kind of single debounce correct for multiple individual ranges/types?
     // TODO: move to ngOnInit(), unsibscribe in destroy?
     this.subject.debounceTime(400).subscribe((event) => {
-      for (let model of (event.id !== null ? [this.models[event.id]] : this.models)) {
+      for (let model of (event.id !== undefined ? [this.models[event.id]] : this.models)) {
         switch (event.type) {
         case 'speed':
           cu.setSpeed(model.id, model.speed);
@@ -58,14 +53,12 @@ export class TuningPage {
       }
     });
   }
-
-  update(type: string, value: number, id = null) {
-    if (id === null) {
-      this.models.forEach(model => {
-        model[type] = value;
-      });
+  
+  update(type: string, value: number, id?: number) {
+    for (let model of (id !== undefined ? [this.models[id]] : this.models)) {
+      model[type] = value;
     }
-    this.ref.detectChanges();
     this.subject.next({id: id, type: type});
+    this.ref.detectChanges();
   }
 }
