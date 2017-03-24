fbf95da964152bcd85ee393ee5d1484305b677d5
Thomas Kemmer <tkemmer@computer.org>
2017-03-24 13:31:21 +0100

    Fix #40: Add IonicErrorHandler.

--- src/logging/error-handler.ts
+++ src/logging/error-handler.ts
@@ -1,12 +1,18 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 
+import { IonicErrorHandler } from 'ionic-angular';
+
 import { Logger } from './logger';
 
 @Injectable()
 export class LoggingErrorHandler implements ErrorHandler {
+  private defaultHandler = new IonicErrorHandler();
+
   constructor(private logger: Logger) {}
 
-  handleError(error) {
+  handleError(error: any) {
+    // FIXME: this will write to console twice
+    this.defaultHandler.handleError(error);
     this.logger.error('Error:', error);
   }
 }
