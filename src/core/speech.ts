a19d387fa918dd30ea4deb31b1039b096811cbe4
Thomas Kemmer <tkemmer@computer.org>
2017-09-27 22:56:39 +0200

    Adjust TTS rate on iOS.

--- src/core/speech.ts
+++ src/core/speech.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 
+import { Platform } from 'ionic-angular';
+
 import { Logger } from './logger';
 
 @Injectable()
@@ -13,9 +15,12 @@ export class Speech {
 
   private pending = 0;
 
-  private rate = 1.0;
+  private rate: number;
 
-  constructor(private tts: TextToSpeech, private logger: Logger) {}
+  constructor(private tts: TextToSpeech, private logger: Logger, platform: Platform) {
+    // See https://github.com/vilic/cordova-plugin-tts/issues/40
+    this.rate = platform.is('ios') ? 1.5 : 1.0;
+  }
 
   setLocale(locale: string) {
     this.locale = locale;
@@ -31,7 +36,7 @@ export class Speech {
     this.pending++;
     this.promise = this.promise.then(() => {
       if (--this.pending === 0) {
-        return this.tts.speak({text: message, locale: this.locale || 'en-us', rate: this.rate || 1.0});
+        return this.tts.speak({text: message, locale: this.locale || 'en-us', rate: this.rate});
       } else {
         this.logger.warn('Speech cancelled', message);
       }
