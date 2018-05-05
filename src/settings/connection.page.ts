b5a4f61df351b30abba3001faafed50bae291167
Thomas Kemmer <tkemmer@computer.org>
2018-05-05 20:40:42 +0200

    Fix #93: Minor UI improvements.

--- src/settings/connection.page.ts
+++ src/settings/connection.page.ts
@@ -2,6 +2,15 @@ import { Component, OnDestroy } from '@angular/core';
 
 import { Settings, Connection } from '../core';
 
+function isObjectSubset(a, b) {
+  for (let key in a) {
+    if (a[key] !== b[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 @Component({
   templateUrl: 'connection.page.html'
 })
@@ -9,9 +18,13 @@ export class ConnectionPage implements OnDestroy {
 
   connection = new Connection();
 
-  reconnectDelay = {
-    lower: 1000,
-    upper: 10000
+  ranges = {
+    connection: 5000,
+    request: 1000,
+    reconnect: {
+      lower: 2000,
+      upper: 10000
+    }
   };
 
   private subscription: any;
@@ -21,17 +34,27 @@ export class ConnectionPage implements OnDestroy {
   ngOnInit() {
     this.subscription = this.settings.getConnection().subscribe(connection => {
       this.connection = connection;
-      this.reconnectDelay = {
-        lower: connection.minReconnectDelay,
-        upper: connection.maxReconnectDelay
+      this.ranges = {
+        connection: connection.connectionTimeout,
+        request: connection.requestTimeout,
+        reconnect: {
+          lower: connection.minReconnectDelay,
+          upper: connection.maxReconnectDelay
+        }
       };
     });
   }
 
   ngOnDestroy() {
-    this.connection.minReconnectDelay = this.reconnectDelay.lower;
-    this.connection.maxReconnectDelay = this.reconnectDelay.upper;
-    this.settings.setConnection(this.connection);
+    const connection = {
+      connectionTimeout: this.ranges.connection,
+      requestTimeout: this.ranges.request,
+      minReconnectDelay: this.ranges.reconnect.lower,
+      maxReconnectDelay: this.ranges.reconnect.upper
+    };
+    if (!isObjectSubset(connection, this.connection)) {
+      this.settings.setConnection(Object.assign({}, this.connection, connection));
+    }
     this.subscription.unsubscribe();
   }
 }
