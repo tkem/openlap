86b8a7ed1a3672c055302e645d4d908a90c68892
Philipp Anné <philipp@annee.de>
2019-01-08 22:28:41 +0100

    Colorize gauge according to value.

--- src/shared/gauge.component.ts
+++ src/shared/gauge.component.ts
@@ -9,9 +9,29 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 })
 export class GaugeComponent {
 
-    @Input() min: number = 0;
+  style: {[key: string]: string} = {};
 
-    @Input() max: number = 1.0;
+   _min: number = 0;
+   _max: number = 1.0;
+   _value: number;
 
-    @Input() value: number;
+    @Input() set min(value: number) { this._min = value; this.calcStyle(); } 
+
+    @Input() set max(value: number) { this._max = value; this.calcStyle(); } 
+
+    @Input() set value(v: number) { this._value = v; this.calcStyle(); } 
+
+    calcStyle() {
+
+      let p = (this._value - this._min) / (this._max - this._min);
+
+      let r = (Math.trunc(255 * (p > 0.5 ? 1.0 - 2.0 * (p - 0.5) : 1.0))).toString(16);
+      let g = (Math.trunc(255 * (p > 0.5 ? 1.0 : 2.0 * p))).toString(16);
+      
+      r = r.length == 2 ? r : '0' + r;
+      g = g.length == 2 ? g : '0' + g;
+      
+      this.style['padding-right'] = 100 * (this._max - this._value) / (this._max - this._min) + '%';
+      this.style['background-color'] = '#' + r + g + '00';
+    }
 }
