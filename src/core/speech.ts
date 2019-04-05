7826472b258c74022f8f9917a12c76cc1b385859
Thomas Kemmer <tkemmer@computer.org>
2019-04-05 20:54:05 +0200

    Add Web Speech support.

--- src/core/speech.ts
+++ src/core/speech.ts
@@ -1,27 +1,88 @@
 import { Injectable } from '@angular/core';
 
-import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { TextToSpeech, TTSOptions } from '@ionic-native/text-to-speech';
 
 import { Platform } from 'ionic-angular';
 
 import { Logger } from './logger';
 
+class WebSpeech {
+  constructor(private speech: SpeechSynthesis, logger: Logger) {
+    logger.debug('Using Web Speech API');
+  }
+
+  speak(textOrOptions: string | TTSOptions): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const utterance = this['__utterance__'] = new SpeechSynthesisUtterance();
+      if (typeof textOrOptions === 'string') {
+        utterance.text = textOrOptions;
+      } else {
+        utterance.text = textOrOptions.text;
+        utterance.lang = textOrOptions.locale;
+        utterance.rate = textOrOptions.rate;
+      }
+      utterance.onend = () => {
+        resolve();
+      };
+      utterance.onerror = e => {
+        reject(e);
+      }
+      try {
+        this.speech.speak(utterance);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  stop(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      try {
+        this.speech.cancel();
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+}
+
+class DummySpeech {
+  constructor(logger: Logger) {
+    logger.info('Speech not supported on this platform');
+  }
+
+  speak(textOrOptions: string | TTSOptions): Promise<void> {
+    return Promise.resolve();
+  }
+
+  stop(): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
 @Injectable()
 export class Speech {
 
-  private locale = 'en-us';
+  private locale = 'en-US';
 
   private promise = Promise.resolve();
 
   private pending = 0;
 
-  private rate: number;
+  private rate = 1.0;
 
   private lastMessage: string;
 
   constructor(private tts: TextToSpeech, private logger: Logger, platform: Platform) {
-    // See https://github.com/vilic/cordova-plugin-tts/issues/40
-    this.rate = platform.is('ios') ? 1.5 : 1.0;
+    if (platform.is('cordova')) {
+      // See https://github.com/vilic/cordova-plugin-tts/issues/40
+      this.rate = platform.is('ios') ? 1.5 : 1.0;
+    } else if (window.speechSynthesis) {
+      this.tts = new WebSpeech(window.speechSynthesis, logger);
+    } else {
+      this.tts = new DummySpeech(logger);
+    }
   }
 
   setLocale(locale: string) {
