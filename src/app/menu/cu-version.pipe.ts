b1898c811e185e8267ec204b4c7274fa6bb17ec9
Thomas Kemmer <tkemmer@computer.org>
2026-06-16 21:03:52 +0200

    Show CU firmware version as four-digit number.

--- src/app/menu/cu-version.pipe.ts
+++ src/app/menu/cu-version.pipe.ts
@@ -7,6 +7,9 @@ import { Injectable, Pipe, PipeTransform } from '@angular/core';
 @Injectable()
 export class CuVersionPipe implements PipeTransform {
   transform(value: string): string {
-    return value ? value.replace(/^5(\d)(\d+)$/, '$1.$2') : '...';
+    // newer CUs use inital "8" instead of "5" for hardware variant, 
+    // and 4-digit numbers seem to be more custom within the community
+    // return value ? value.replace(/^5(\d)(\d+)$/, '$1.$2') : '...';
+    return value || '????';
   }
 }
